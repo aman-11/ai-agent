b98ff8173664645e1907e33aaee430d6c68c4288
aayushProvusInc <aayush@provusinc.com>
2025-02-04 02:09:21 +0530

    feat: clerk with convex

--- components/ConvexClientProvider.tsx
+++ components/ConvexClientProvider.tsx
@@ -1,10 +1,26 @@
+'use client';
+
+import { ClerkProvider, useAuth } from "@clerk/nextjs";
+import { ConvexProviderWithClerk } from "convex/react-clerk";
+import { ConvexReactClient } from "convex/react";
+
+/**
+ * The convex being at backend will read the env variables from the server .env file
+ */
+const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL!);
+
 export default function ConvexClientProvider({
-     children,
- }: Readonly<{
-    children: React.ReactNode;
+  children,
+}: Readonly<{
+  children: React.ReactNode;
 }>) {
-    return (
-        <ConvexClientProvider>
-        </ConvexClientProvider>
-    );
+  return (
+    <ClerkProvider
+      publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY!}
+    >
+      <ConvexProviderWithClerk client={convex} useAuth={useAuth}>
+        {children}
+      </ConvexProviderWithClerk>
+    </ClerkProvider>
+  );
 }
